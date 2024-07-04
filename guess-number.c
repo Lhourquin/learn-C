3e019569949c11d45ac53b1e5c034ce8fb5891ee
lhourquin <lhourquin@gmail.com>
2024-07-04 18:37:03 +0200

    guess-number: use rand function

--- guess-number.c
+++ guess-number.c
@@ -1,17 +1,19 @@
+#include <stdlib.h>
 #include <stdio.h>
 
 int main(void) {
 	char answer;//char value must be declared with '' and not "", the quotation marks is for string.
-	int number = 5;
+	const int MAX = 100, MIN = 8;
+	int mysterious_number = (rand() % (MAX  - MIN + 1)) + MIN;
 	int value = 'y';
 	printf("Do you want play a game with me ? (y/n)");
 	scanf("%c",&answer);
 	switch(answer){
 		case 'y':
-			while(value != number){
+			while(value != mysterious_number){
 				printf("Guess the number (type 0 if you want to stop the game)");
 				scanf("%d",&value);
-				if(value != number && value != 0){
+				if(value != mysterious_number && value != 0){
 					printf("You typing the wrong number\n");
 				}else if(value == 0){
 					printf("GoodBye.\n");
