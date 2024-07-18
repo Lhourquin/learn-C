e5f4a6e373ded8bb172c58a70b6b533fd6a118ef
lhourquin <lhourquin@gmail.com>
2024-07-18 17:59:42 +0200

    struct, notes:     pratice and notes about enum type, and conclusion of the lesson

--- struct.c
+++ struct.c
@@ -7,11 +7,14 @@
 		* enumeration
 	
 */
+void initialize_coords(int *pt_x, int *pt_y, int x,int  y);
+void another_initialzer_coords(Another_coordinates *coords, int x, int y);
 int main(void){
 //structure
 /*
 	What is a struture ?
 	
+
 	A structure is a set of variable could have different types.
 	Unlike an array, is not obligatory to have all data of a same type,
 	you can create a structure including variable of type, char, int and double at the same time.
@@ -61,8 +64,145 @@ int main(void){
 		* The last two have int type to store age and Boolean to check if is sutdent or not.
 	Not limit of variable if we want to add variable in structure to complete him.
 */	
-	//use struct in function
+
+
+/*
+
+	struct Person user;
+	printf("what is your first name?\n");
+	scanf("%s", user.first_name);
+	printf("what is your last name?\n");
+	scanf("%s", user.last_name);
+	
+	printf("Your first name is %s and your last name is %s\n", user.first_name, user.last_name);
+	struct Person users[2];
+	
+	printf("what is your first name?\n");
+	scanf("%s", users[0].first_name);
+	printf("what is your last name?\n");
+	scanf("%s", users[0].last_name);
+	
+	printf("Your first name is %s and your last name is %s\n", users[0].first_name, users[0].last_name);
+
+	printf("what is your first name?\n");
+	scanf("%s", users[1].first_name);
+	printf("what is your last name?\n");
+	scanf("%s", users[1].last_name);
+	
+	printf("Your first name is %s and your last name is %s\n", users[1].first_name, users[1].last_name);
+	printf("first user named %s and this last name is %s, the second user is %s and this last name is %s\n", users[0].first_name, users[0].last_name,users[1].first_name, users[1].last_name );
+
+	Player players[2];
+	size_t number_of_players = sizeof(players) / sizeof(players[0]);
+	printf("%zu\n", number_of_players);
+	int i = 0;
+	for(i = 0; i < 5; i++){
+		int number_user = i + 1;
+		printf("name of player %d\n",number_user);
+		scanf("%s",players[i].name);
+//		printf("Name of the player %d is %s\n", number_user, players[i].name);
+	}
+	
+	for(i = 0; i < 5; i++){
+		int number_user = i + 1;
+		printf("Name of the player %d is %s\n", number_user, players[i].name);
+	}
+
+
+*/
+
+
+	//init structure
+/*
+
+	Like variable, pointer and array, is recommended to initialize it at creation 
+	this avoid garbage value or random data in it. When we declare variable without value,
+	this variable take what they found in memory, where is placed. Sometimes is 0,
+	sometimes is a residue of another program who run before, that why if we print the non inialized variable,
+	we have value with non sense like -93824 or 123428357 etc.(I'm asked earlier in a file, know I know why !!!!!!!!!!!!)
+
+	How we initialize a:
+		* A variable: with 0 (the most simple case)
+		* A pointer: with NULL, NULL is a #define located in <stdlib.h> who generaly equal to 0,
+		but we continue to use NULL by convention for a pointer to clearly see that a pointer and not an ordinary varaible.
+		* An array: each value to 0.
+		* A struct: looks like a litle bit of an array.
+	
+*/
+	//for instance
+ 	Another_coordinates coords = {0,0};
+	/*
+		That define in order:
+			- coords.x = 0;
+			- coords.y = 0;
+	*/
+
+
+	//use pointer on a struct
 	
 	
+	initialize_coords(&coords.x, &coords.y, 23, 45);//by using variable of a struct, nothing change, we make like we see earlier
+	printf("coords_initialized.x %d\n", coords.x);
+
+	Another_coordinates another_point_2 = {0,0};
+	another_initialzer_coords(&another_point_2, 90, 34);//that really interesting, here, we send directly the address of the struct of Antoher_coordinates type.
+	printf("antoher_point_2.x: %d\n", another_point_2.x);
+	printf("antoher_point_2.y: %d\n", another_point_2.y);
+
+	//enum
+	Volume sound = MEDIUM;//as we define in struct.h, we initialize a variable of type Volume, and we choose to use MEDIUM
+	printf("sound: %d\n", sound);
+/*  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+	output is 1, why ? 
+	First, notice the ressemblence with the use of preprocessor constant, is similar but not exactly the same thing.
+	The compiler associate automatically a possible value for variable in the enum type.
+	In the case of our Volume enum type:
+		* WEAK = 0
+		* MEDIUM = 1
+		* STRONG = 2
+	The automatic initialization begin at 0.
+	
+	Unlike define, is the compiler himself who initialize MEDIUM to 1.
+	And not the preprocessor (first step of compilation)
+	At the end, is the same thing
+	When we initalize sound to MEDIUM, we add the value 1 in the memory case.
+	
+	It's useful to know if MEDIUM = 1 or WEAK = 0 ?
+	Not really. the compiler associate the value for us automatically, so when we use is in a comparison, we can write this:
+*/
+	if(sound == MEDIUM){//is really useful, because that allows for us to write clearly code like this, no matter the value will be.
+		puts("sound is medium.");
+	}else{
+		puts("sound in not medium.");
+	}
+
+//value associated directly at dlecaration of enum type Another_volume
+	Another_volume video_sound = MEDIUM_2;
+	printf("video_sound: %d\n", video_sound);
+
 	return 0;
+
+	
 }
+
+void initialize_coords(int *pt_x, int *pt_y, int x, int y){
+	*pt_x = x;//access to the variable of the pointer to assignate the value we want
+	*pt_y = y;
+}
+void another_initialzer_coords(Another_coordinates *coords, int x, int y){//here in arguments, we add the type of the pointer
+	(*coords).x = x;//first syntax: we cannot do like a simple variable, here we need to use perenthese, because if we use just the point like in variable, we apply a separation on the word coords and not on coords entirely. What we want, is to access at *coords to modify they value
+	coords->y = y;//second and most used syntax, we can use this syntax only on a pointer, not on normaly variable.
+}
+
+
+/*
+	Conclusion:
+
+	 	* struct is a type of variable we can customize, unlike predifened type, we need to define by oursleve.
+		* a struct is composed by subvariable generally with basic type like int, char, double etc. even array.
+		* we can access to this variable of the struct by separate the name of the variable of the type of struct by a pont '.'
+		* if we handle a pointer, we use these syntact:
+			- (*pt).x
+			- pt->x (the best and most used)
+		* an enum is a type of custom variable who can take only one of predefined value like: WEAK, MEIDUM, STRONG for instance.
+*/
--- struct.h
+++ struct.h
@@ -19,4 +19,34 @@ struct Another_coordinates{
 	int x;
 	int y;
 };
+
+typedef struct Player Player;
+struct Player{
+	char name[100];
+};
+
+//enum
+/*
+	enum is a different way to create our custom type fo variables.
+	enum not have sub variable like a struct.
+	enum take only one case in memory,
+	and this case can take one of value you define (at only once)
+*/
+//example
+typedef enum Volume Volume;
+enum Volume{
+	WEAK, MEDIUM, STRONG
+};
+//associate a specific value to our variable
+/*
+	Why is useful to associate specific value in enum ?
+
+	Becaus, like ine the case of Volume, if on the computer the volume is handle between 0 and 100, it is useful to associate a specific value to each element
+*/
+typedef enum Another_volume Another_volume;
+enum Another_volume{
+	MUTE = 0, WEAK_2 = 10, MEDIUM_2 = 50, STRONG_2 = 100
+};
+//If we not define the next element, it will be equal to the value of the previous element + 1
+//we need to create new name of variable, we cannot use the same in another enum type ??
 #endif
