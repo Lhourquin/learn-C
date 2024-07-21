57a5505e7db342c763ed61e155cd4eb61e995d08
lhourquin <lhourquin@gmail.com>
2024-07-22 01:17:36 +0200

    memory-allocation, notes: freeing memory manualy, explanation of behavior of malloc anf free function, explanation of two way to allocate memory

--- dynamic-allocation.c
+++ dynamic-allocation.c
@@ -15,11 +15,48 @@ int main(void){
 //Request memory with malloc (dynamic/manually allocation)
 	int *age = NULL;
 	age = malloc(sizeof(int));
+/*
+	malloc prototype:
+	void* malloc(size_t number_of_necesary_bytes);
+	these function retrun a pointer empty of any type, and thses parametter is the size we need, we can use sizeof function with the type in parametters to specify the number of bytes we want to use, in this case is type of int, so it will take 4 bytes in memory
+*/
+
+
 	if(age == NULL){
-		exit(0);
+		exit(0);//if is NULL, stop immediatly the program
 	}
 	printf("How old are you ?");
 	scanf("%d", age);
 	printf("You have %d years old\n", *age);
+/*
+	freeing memory:
+	like fclose, the free function will freeing memeory after we finish to use and we no longer need to use this.
+	their prototype:
+	void free(void* pointer);
+	it take in parametter the pointer we have used with malloc earlier.		
+	
+*/
+	free(age);// we no longer need memory, we freeing it manally like this.
+/*
+	age in a pointer, we don't use it at the same way with a real variable.
+		* to get the value of the variable, we put before them the symbol * like we see earlier in the course about pointer
+		* to specify these address, we just puts the name of the pointer in the function, without the symbol &
+	
+*/
+//	what we doing is like doing this ⬇️ ****
+	int another_age = 0;//allocate memory to the variable of type int, will take 4 bytes.
+	//using of the memory
+	printf("How old are you ?");
+	scanf("%d", &another_age);
+	printf("You have %d years old\n", another_age);
+	
+	
 	return 0;
-}
+}//**** freeing memory no longer need automatically
+
+/*
+	Conclusion:
+	we have two way to allocate memory:
+		* manualy by using malloc() and free() function
+		* automatically by declaration of a variable
+*/
