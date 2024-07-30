65eec5efc32b14181a8be6885519ad97571315a6
lhourquin <lhourquin@gmail.com>
2024-07-30 08:13:01 +0200

    linked list, refacto

--- linked-list.c
+++ linked-list.c
@@ -4,107 +4,48 @@
 /*
 	the problem with array: there are immutable,	
 	we cannot enlarge or chnage the size after this creation,
-	to do that we do creating another.
-	
+	to do that we do creating another.	
 	C language not offer another way to store data,
 	by is possible to create our custom system to do this.
 	This is why we will see in this course.
 */
 /*
-	Linked List:
+	Linked Linked_List:
 	
-	The Linked List is a way to organize a set of data in memory,
+	The Linked Linked_List is a way to organize a set of data in memory,
 	That consist to gather structures by linked it between them by using pointer.
-	A Linked List is an assembly of structure linked by pointer.
+	A Linked Linked_List is an assembly of structure linked by pointer.
 	Each element can contains what we want: one or many int, double, etc ..
 	And of top of that, each element have a pointer to the next element.
-	That form a list of pointer, that why is named Linked List
+	That form a list of pointer, that why is named Linked Linked_List
 */
 /*
 	Contrary of array, the elements of a linked list are not placed side by side in memory.
 	Each case pointed to another case in memory, who is not necessary stored right to them.
 */
-List *initialization(void);
-void insertion(List *list, int new_number);
-void remove_in_list(List *list);
-void display_list(List *list);
+Linked_List *initialization(void);
+void insert_at_begin(Linked_List *list, int new_value);
+void remove_in_list(Linked_List *list);
+void display_list(Linked_List *list);
+void insert_in_end(Linked_List *list, int new_value, Node *pt_previous_elem);
+Node* create_node(int value);
+void add_new_node(Linked_List *list, int value);
+
 int main(void){
-	List *my_list = initialization();
+	Linked_List *my_list = malloc(sizeof(*my_list));
+	
+	if(my_list == NULL){
+		exit(EXIT_FAILURE);
+	}
+
+	Node *elem_list = create_node(56);
 	puts("before insertion");
-	printf("%d\n", my_list->prime_number->number);
-	insertion(my_list, 5);
+	//printf("%d\n", my_list->head->value);
+	add_new_node(my_list, 45);
 	puts("after insertion");
-	printf("%d\n", my_list->prime_number->number);
-	puts("second insertion");
-	insertion(my_list, 15);
-	printf("%d\n", my_list->prime_number->number);
-	puts("next element, output should be 5 ?");
-	printf("%d\n", *my_list->prime_number->next);
-	puts("last insertion");
-	insertion(my_list, 35);
-	printf("%d\n", my_list->prime_number->number);
-	puts("next element, output should be 15 ?");
-	printf("%d\n", *my_list->prime_number->next);
-	puts("prime_number element now");
-	printf("%d\n", my_list->prime_number->number);
-	printf("remove prime_number element %d ", my_list->prime_number->number); 
-	remove_in_list(my_list);
-	printf("is replaced by ", my_list->prime_number->number);
-	printf("%d\n", my_list->prime_number->number);
-	display_list(my_list);
+	printf("%d\n", my_list->head->value);
+	print_singly_linked_list(my_list);
 	free(my_list);
 	
 	return 0;
-}
-
-List *initialization(void){
-	List *list = malloc(sizeof(*list));//allowed memeory to list who attempt to store element
-	Element *element = malloc(sizeof(*element));//element with number for the value and next for the pointer
-
-	if(list == NULL || element == NULL){
-		exit(EXIT_FAILURE);
-	} 
-
-	element->number = 0;//initalize value for number
-	element->next = NULL;
-	list->prime_number = element;//prime_number = {int number, Element *next}
-
-	return list;
-}
-
-void insertion(List *list, int new_number){
-	Element *new_elem = malloc(sizeof(*new_elem));//allowed memory to add new element
-	
-	if(list == NULL || new_elem == NULL){
-		exit(EXIT_FAILURE);
-	}
-	new_elem->number = new_number;//new value for number of list (my_list->prime_number->number)
-	new_elem->next = list->prime_number;//pointer to next value (NULL by default)
-	list->prime_number = new_elem;//new element on the list
-}
-
-void remove_in_list(List *list){
-	if(list == NULL){
-		exit(EXIT_FAILURE);
-	}
-
-	if(list->prime_number != NULL){
-		Element *to_remove = list->prime_number;//value need to freeing in memory
-		list->prime_number = list->prime_number->next;//next value swapped to the prime_number removed
-		free(to_remove);//free memory for the previous prime_number value no longer need
-	}
-}
-
-void display_list(List *list){
-	if(list == NULL){
-		exit(EXIT_FAILURE);
-	}
-
-	Element *actual = list->prime_number;
-
-	while(actual != NULL){
-		printf("actual: %d\n", actual->number);
-		actual = actual->next;
-	}
-	printf("NULL\n");
 }
--- linked-list.h
+++ linked-list.h
@@ -1,15 +1,72 @@
+/*
+=== Basic of Linked Lists ===
+
+Node:
+	- The basic unit of a linked list is called a "node"
+	- Each node contains:
+		* a value (data)
+		* a pointer (or reference) to the next data in the list
+Head:
+	- The head is a pointer that points to the first node in the linked list.
+	- If the list is empty, the head points to NULL or nullptr
+Next:
+	- In each node, the next pointer refers to the next node in the sequence.
+	- The last node in the list has its next pointer set to NULL, indicating the end of the list.
+
+
+=== Types of Linked Lists ===
+
+Singly Linked List:
+	- Each node points to the next node
+	- You can traverse the list in one direction (from head to end)
+Doubly Linked List:
+	- Each node contains two pointers: one to the next and one to the previous node.
+	- You can traverse the list in both directions (forward and backward)
+Circular Linked List:
+	- The last node points back to the head, forming a circle
+	- Can be singly or doubly linked
+*/
 #ifndef LINKED_LIST_H
 #define LINKED_LIST_H
 
-typedef struct Element Element;
-struct Element{//will be element of the list
-	int number;
-	Element *next;
+//Singly Linked List example
+typedef struct Node Node;
+struct Node{
+	int value;
+	Node *next;
 };
 
-typedef struct List List;
-struct List{//the list who have element
-	Element *prime_number;
+typedef struct Linked_List Linked_List;
+struct Linked_List{
+	Node *head;
 };
 
+Node* create_node(int value){
+	Node *new_node = malloc(sizeof(new_node));
+	new_node->value = value;
+	new_node->next = NULL;
+	return new_node;
+}
+
+void add_new_node(Linked_List *list, int value){
+	Node *new_node = malloc(sizeof(new_node));
+	
+	new_node->value = value;
+	new_node->next = NULL;
+	list->head = new_node;
+}
+
+void print_singly_linked_list(Linked_List *list){
+	Node *actual = list->head;
+
+	if(list == NULL){
+		exit(EXIT_FAILURE);
+	}
+	
+	while(actual != NULL){
+		printf(" %d -> ", actual->value);
+		actual = list->head->next;
+	}
+	printf("NULL\n");
+}
 #endif
